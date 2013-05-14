3c988947599446925a4f6d74d038c7d935b607c0
Stephan Frank <defclass@googlemail.com>
2013-05-14 20:56:08 +0200

    benchmarks for rational arithmetic

--- bench.lisp
+++ bench.lisp
@@ -199,7 +199,89 @@
                           (format *stream* "Test PASSED~2%")
                           (format *stream* "Test FAILED~2%"))))))))
 
+(defun bench-q+ () ; limbs: always
+  (macrolet ((tstfun (f a b)
+               `(lambda ()
+                  (loop for i in ,a
+                        for j in ,b
+                        collect (,f i j)))))
+    (loop for limbsa fixnum from 2
+          for limbsb fixnum from 1
+          with gmp-win = 0
+          until (or (= gmp-win 5)
+                    (= limbsa 50))
+          do
+             (loop 
+               for i below 10000
+               collect (/ (sb-gmp:random-bitcount *state* (* limbsa sb-vm:n-word-bits))
+                          (sb-gmp:random-bitcount *state* (* limbsb sb-vm:n-word-bits)))
+                 into list-a
+               collect (/ (sb-gmp:random-bitcount *state* (* limbsa sb-vm:n-word-bits))
+                          (sb-gmp:random-bitcount *state* (* limbsb sb-vm:n-word-bits)))
+                 into list-b
+               finally
+                  (let (time1 time2)
+                    (let ((r-sbcl (progn (sb-ext:gc)
+                                         (sb-ext:call-with-timing 
+                                          (lambda (&rest plist) 
+                                            (setf time1 plist))
+                                          (tstfun + list-a list-b))))
+                          (r-gmp (progn (sb-ext:gc)
+                                        (sb-ext:call-with-timing 
+                                         (lambda (&rest plist) 
+                                           (setf time2 plist))
+                                         (tstfun sb-gmp:mpq-add list-a list-b)))))
+                      (format *stream* "limbs: ~s~%Time SBCL: ~s~%Time GMP:  ~s~%"
+                              limbsa time1 time2)
+                      (when (< (getf time2 :PROCESSOR-CYCLES)
+                               (getf time1 :PROCESSOR-CYCLES))
+                        (incf gmp-win))
+                      (if (= (length r-sbcl) (length r-gmp))
+                          (format *stream* "Test PASSED~2%")
+                          (format *stream* "Test FAILED~2%"))))))))
+
+(defun bench-q* () ; limbs: always
+  (macrolet ((tstfun (f a b)
+               `(lambda ()
+                  (loop for i in ,a
+                        for j in ,b
+                        collect (,f i j)))))
+    (loop for limbsa fixnum from 2
+          for limbsb fixnum from 1
+          with gmp-win = 0
+          until (or (= gmp-win 5)
+                    (= limbsa 50))
+          do
+             (loop 
+               for i below 10000
+               collect (/ (sb-gmp:random-bitcount *state* (* limbsa sb-vm:n-word-bits))
+                          (sb-gmp:random-bitcount *state* (* limbsb sb-vm:n-word-bits)))
+                 into list-a
+               collect (/ (sb-gmp:random-bitcount *state* (* limbsa sb-vm:n-word-bits))
+                          (sb-gmp:random-bitcount *state* (* limbsb sb-vm:n-word-bits)))
+                 into list-b
+               finally
+                  (let (time1 time2)
+                    (let ((r-sbcl (progn (sb-ext:gc)
+                                         (sb-ext:call-with-timing 
+                                          (lambda (&rest plist) 
+                                            (setf time1 plist))
+                                          (tstfun * list-a list-b))))
+                          (r-gmp (progn (sb-ext:gc)
+                                        (sb-ext:call-with-timing 
+                                         (lambda (&rest plist) 
+                                           (setf time2 plist))
+                                         (tstfun sb-gmp:mpq-mul list-a list-b)))))
+                      (format *stream* "limbs: ~s~%Time SBCL: ~s~%Time GMP:  ~s~%"
+                              limbsa time1 time2)
+                      (when (< (getf time2 :PROCESSOR-CYCLES)
+                               (getf time1 :PROCESSOR-CYCLES))
+                        (incf gmp-win))
+                      (if (= (length r-sbcl) (length r-gmp))
+                          (format *stream* "Test PASSED~2%")
+                          (format *stream* "Test FAILED~2%"))))))))
+
 (defun bench ()
   (let ((*state* (sb-gmp:make-gmp-rstate)))
     (sb-gmp:rand-seed *state* 1234)
-    (bench-*)))
+    (bench-q*)))
