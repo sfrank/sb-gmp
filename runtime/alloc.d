d80da431e2746249a802db636736f4e4ea5e1c9a
Stephan Frank <defclass@googlemail.com>
2014-07-16 22:15:31 +0200

    remove errornously committed binaries

--- runtime/alloc.d
+++
@@ -1,7 +0,0 @@
-alloc.o alloc.d : alloc.c sbcl.h genesis/config.h genesis/constants.h runtime.h \
- fixnump.h os.h target-os.h target-arch-os.h target-arch.h interr.h \
- alloc.h gc-internal.h genesis/simple-fun.h thread.h globals.h \
- runtime-options.h gencgc-alloc-region.h gc.h genesis/symbol.h \
- genesis/static-symbols.h genesis/thread.h genesis/fdefn.h interrupt.h \
- validate.h thread.h gencgc-internal.h genesis/code.h pseudo-atomic.h \
- genesis/vector.h genesis/cons.h genesis/bignum.h genesis/sap.h
